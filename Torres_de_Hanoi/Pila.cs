284db6a90e04fafbcbb2737c0c5f1f0aa4f00434
David <deividgf567@mail.com>
2022-03-20 23:53:39 +0100

    Clase Pila

--- Torres_de_Hanoi/Pila.cs
+++ Torres_de_Hanoi/Pila.cs
@@ -6,38 +6,81 @@ using System.Threading.Tasks;
 
 namespace Torres_de_Hanoi
 {
-    class Pila
+class Pila
     {
-        public int Size { get; set; }
-        /* TODO: Elegir tipo de Top
-        public int Top { get; set; }
-        public String Top { get; set; }        
-        */
-        /* TODO: Elegir tipo de Elementos
-        public Disco[] Elementos { get; set; }
-        public List<Disco> Elementos { get; set; }
-        */
+        public string Nombre { get; set; }
 
-        /* TODO: Implementar métodos */
-        public Pila()
+        public List<Disco> Elementos { get; set; }
+
+
+        public int Size 
+        { get
+            {
+                return Elementos.Count;
+            }
+        }
+
+        public Disco Top 
+        { get 
+            {
+                if(isEmpty() == false)
+                {
+                    return Elementos[Size - 1];
+                }
+                return null;
+            }
+        }
+
+
+        /* Constructores */
+        public Pila(string n)
         {
+            Elementos = new List <Disco> ();
+            Nombre = n;
 
         }
+
+        public Pila(int cantidad, string n)
+        {
+            Elementos = new List<Disco> ();
+            Nombre = n;
+
+            for(int i=cantidad; i>0; i--)
+            {
+                //Se crea un disco para añadir a la pila
+                Disco nuevo = new Disco();
+                nuevo.tamaño = i;
+
+                //Se añade el disco a la pila
+                push(nuevo);
+            }
+        }
+        / -------------------- */
 
         public void push(Disco d)
         {
-
+            Elementos.Add(d);
         }
+
 
         public Disco pop()
         {
-            return null;
-        }                
+            //Guardamos disco de arriba para no "perderlo"
+            Disco d = Top;
+
+            //Eliminar disco
+            Elementos.RemoveAt(Size - 1);
+
+            return d;
+        }
 
         public bool isEmpty()
         {
-            return true;
+           if(Size == 0)
+            {
+                return true;
+            }
+           return false;
         }
 
-    }
 }
