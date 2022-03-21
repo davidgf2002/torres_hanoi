31d1b93beff4613c643a3ad6654e078f4a1d1d6a
David <deividgf567@mail.com>
2022-03-21 05:28:57 +0100

    Mover Disco

--- Torres_de_Hanoi/Hanoi.cs
+++ Torres_de_Hanoi/Hanoi.cs
@@ -8,10 +8,47 @@ namespace Torres_de_Hanoi
 {
     class Hanoi
     {
-        /*TODO: Implementar métodos*/
-        public void mover_disco(Pila a, Pila b)
-        {
 
+        private static int num_movimientos;
+        private static Pila INI, FIN, AUX;
+
+
+        private static void mover_disco(Pila a, Pila b)
+        {
+            if(!solucion()) //solucion() == false
+            { 
+                // a -> b
+                if(!a.isEmpty())
+                {
+                    if(b.isEmpty())
+                    {
+                        b.push( a.pop() );
+                        num_movimientos ++;
+                        Console.WriteLine(a.Nombre + "-" +b.Top.tamaño + "->" + b.Nombre);
+                    }
+                    else if(a.Top.tamaño < b.Top.tamaño)
+                    {
+                        b.push( a.pop() );
+                        num_movimientos ++;
+                        Console.WriteLine(a.Nombre + "-" + b.Top.tamaño + "->" + b.Nombre);
+                    }
+                    else
+                    {
+                        a.push( b.pop() );
+                        num_movimientos ++;
+                        Console.WriteLine(b.Nombre + "-" + a.Top.tamaño + "->" + a.Nombre);
+                    }
+                }
+                else //a.isEmpty() == true
+                {
+                    if(!b.isEmpty())
+                    {
+                        a.push( b.pop() );
+                        num_movimientos ++;
+                        Console.WriteLine(b.Nombre + "-" + a.Top.tamaño + "->" + a.Nombre);
+                    }
+                }
+            }
         }
 
         public int iterativo(int n, Pila ini, Pila fin, Pila aux)
